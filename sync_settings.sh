11cf3ceea620ac2806f4d52beedb2169804c9c15
Alexandre Montplaisir <alexmonthy@voxpopuli.im>
2017-01-10 17:30:23 -0500

    Update sync_settings script
    "common" doesn't end in .core anymore

--- sync_settings.sh
+++ sync_settings.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 ###############################################################################
-# Copyright (c) 2014 Ericsson
+# Copyright (c) 2014, 2017 Ericsson and others
 #
 # All rights reserved. This program and the accompanying materials
 # are made available under the terms of the Eclipse Public License v1.0
 # which accompanies this distribution, and is available at
 # http://www.eclipse.org/legal/epl-v10.html
-#
-# Contributors:
-#     Alexandre Montplaisir - Initial API and implementation
 ###############################################################################
 
 # Synchronize the project settings for all plugins
@@ -22,7 +19,7 @@ RUNTIME_FILES=$RUNTIME_REFERENCE/.settings/*.prefs
 TEST_FILES=$TEST_REFERENCE/.settings/*.prefs
 
 # Runtime plugins
-for DIR in */*.core */*.ui doc/org.eclipse.tracecompass.examples
+for DIR in */*.core */*.ui */*.common
 do
   # Skip non-directories
   if [ ! -d $DIR ]; then
@@ -42,7 +39,7 @@ done
 
 
 # Test plugins
-for DIR in */*.tests */*.alltests
+for DIR in */*.tests
 do
   # Skip non-directories
   if [ ! -d $DIR ]; then
