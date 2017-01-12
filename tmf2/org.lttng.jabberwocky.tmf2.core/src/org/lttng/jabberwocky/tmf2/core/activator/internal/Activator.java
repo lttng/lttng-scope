1997952cd98f96f80d43caae041fb5bc6c871bd6
Alexandre Montplaisir <alexmonthy@efficios.com>
2017-01-11 19:21:07 -0500

    Introduce tmf2 plugins

---
+++ tmf2/org.lttng.jabberwocky.tmf2.core/src/org/lttng/jabberwocky/tmf2/core/activator/internal/Activator.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (C) 2017 EfficiOS Inc., Alexandre Montplaisir <alexmonthy@efficios.com>
+ *
+ * All rights reserved. This program and the accompanying materials are
+ * made available under the terms of the Eclipse Public License v1.0 which
+ * accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.lttng.jabberwocky.tmf2.core.activator.internal;
+
+import org.eclipse.tracecompass.common.TraceCompassActivator;
+
+/**
+ * Plugin activator
+ */
+public class Activator extends TraceCompassActivator {
+
+    private static final String PLUGIN_ID = "org.eclipse.tracecompass.tmf2.core"; //$NON-NLS-1$
+
+    /**
+     * Return the singleton instance of this activator.
+     *
+     * @return The singleton instance
+     */
+    public static Activator instance() {
+        return (Activator) TraceCompassActivator.getInstance(PLUGIN_ID);
+    }
+
+    /**
+     * Constructor
+     */
+    public Activator() {
+        super(PLUGIN_ID);
+    }
+
+    @Override
+    protected void startActions() {
+    }
+
+    @Override
+    protected void stopActions() {
+    }
+
+}
