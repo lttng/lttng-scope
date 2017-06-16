c14e13c15fa79d99b3d4ec612f14f4ce8ad546af
Alexandre Montplaisir <alexmonthy@efficios.com>
2017-06-16 16:25:14 -0400

    lttng: Model the states of IRQs and SoftIRQs
    These will be modeled using the new attribute aggregation feature, and
    will allow fetching directly the state of any single IRQ or SoftIRQ.
    Just access the new attributes normally, and the library will take care
    of iterating over all known IRQ/SoftIRQ attributes and returning the
    appropriate information.
    
    Note that this slows down full queries noticeably. There is probably
    some work to do in the state system to avoid doing nested IO operations
    (i.e. just fetch all "real" attributes first into memory, then use the
    in-memory structure to resolve aggregates).
    
    Signed-off-by: Alexandre Montplaisir <alexmonthy@efficios.com>

--- lttng/org.lttng.scope.lttng.kernel.core/src/org/lttng/scope/lttng/kernel/core/analysis/os/KernelAnalysisModule.java
+++ lttng/org.lttng.scope.lttng.kernel.core/src/org/lttng/scope/lttng/kernel/core/analysis/os/KernelAnalysisModule.java
@@ -16,6 +16,11 @@ package org.lttng.scope.lttng.kernel.core.analysis.os;
 import static java.util.Objects.requireNonNull;
 import static org.lttng.scope.common.core.NonNullUtils.nullToEmptyString;
 
+import java.util.List;
+import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 import org.eclipse.jdt.annotation.NonNull;
 import org.eclipse.tracecompass.tmf.core.statesystem.ITmfStateProvider;
 import org.eclipse.tracecompass.tmf.core.statesystem.TmfStateSystemAnalysisModule;
@@ -25,7 +30,12 @@ import org.lttng.scope.lttng.kernel.core.trace.IKernelTrace;
 import org.lttng.scope.lttng.kernel.core.trace.layout.ILttngKernelEventLayout;
 import org.lttng.scope.lttng.kernel.core.trace.layout.internal.LttngEventLayout;
 
+import com.google.common.primitives.Ints;
+
 import ca.polymtl.dorsal.libdelorean.ITmfStateSystemBuilder;
+import ca.polymtl.dorsal.libdelorean.aggregation.AttributePriorityAggregationRule;
+import ca.polymtl.dorsal.libdelorean.aggregation.IStateAggregationRule;
+import ca.polymtl.dorsal.libdelorean.statevalue.TmfStateValue;
 
 /**
  * State System Module for lttng kernel traces
@@ -59,6 +69,84 @@ public class KernelAnalysisModule extends TmfStateSystemAnalysisModule {
 
     @Override
     protected void setupAggregationRules(ITmfStateSystemBuilder ss) {
-        // TODO Add IRQ aggregation rules
+        /* Set up the virtual "IRQs" and "SoftIRQs" sub-trees */
+        final int cpusQuark = ss.getQuarkAbsoluteAndAdd(Attributes.CPUS);
+        final List<Integer> cpuQuarks = ss.getSubAttributes(cpusQuark, false);
+
+        Set<Integer> irqNumbers = cpuQuarks.stream()
+            .flatMap(quark -> {
+                int irqsQuark = ss.getQuarkRelative(quark, Attributes.IRQS);
+                List<Integer> irqQuarks = ss.getSubAttributes(irqsQuark, false);
+                return irqQuarks.stream()
+                    .map(irqQuark -> ss.getAttributeName(irqQuark))
+                    .map(name -> Ints.tryParse(name))
+                    .filter(Objects::nonNull);
+            })
+            .collect(Collectors.toSet());
+
+        Set<Integer> softIrqNumbers = cpuQuarks.stream()
+                .flatMap(quark -> {
+                    int irqsQuark = ss.getQuarkRelative(quark, Attributes.SOFT_IRQS);
+                    List<Integer> irqQuarks = ss.getSubAttributes(irqsQuark, false);
+                    return irqQuarks.stream()
+                        .map(irqQuark -> ss.getAttributeName(irqQuark))
+                        .map(name -> Ints.tryParse(name))
+                        .filter(Objects::nonNull);
+                })
+                .collect(Collectors.toSet());
+
+        int irqsQuark = ss.getQuarkAbsoluteAndAdd(Attributes.IRQS);
+        if (irqsQuark == ss.getNbAttributes()) {
+            /*
+             * FIXME If we just created this attribute, make sure we put a null value into
+             * it so that upcoming queries return something. Should be fixed in the state
+             * system library.
+             */
+            ss.modifyAttribute(ss.getStartTime(), TmfStateValue.nullValue(), irqsQuark);
+        }
+        for (int irqNumber : irqNumbers) {
+            int irqQuark = ss.getQuarkRelativeAndAdd(irqsQuark, String.valueOf(irqNumber));
+            List<String[]> irqPaths = cpuQuarks.stream()
+                .map(quark -> {
+                    String[] cpuQuarkPath = ss.getFullAttributePathArray(quark);
+                    String[] irqAttributePath = new String[4];
+                    irqAttributePath[0] = cpuQuarkPath[0];
+                    irqAttributePath[1] = cpuQuarkPath[1];
+                    irqAttributePath[2] = Attributes.IRQS;
+                    irqAttributePath[3] = String.valueOf(irqNumber);
+                    return irqAttributePath;
+                })
+                .collect(Collectors.toList());
+
+            IStateAggregationRule rule = new AttributePriorityAggregationRule(ss, irqQuark, irqPaths);
+            ss.addAggregationRule(rule);
+        }
+
+        int softIrqsQuark = ss.getQuarkAbsoluteAndAdd(Attributes.SOFT_IRQS);
+        if (softIrqsQuark == ss.getNbAttributes()) {
+            /*
+             * FIXME If we just created this attribute, make sure we put a null value into
+             * it so that upcoming queries return something. Should be fixed in the state
+             * system library.
+             */
+            ss.modifyAttribute(ss.getStartTime(), TmfStateValue.nullValue(), softIrqsQuark);
+        }
+        for (int softIrqNumber : softIrqNumbers) {
+            int softIrqQuark = ss.getQuarkRelativeAndAdd(softIrqsQuark, String.valueOf(softIrqNumber));
+            List<String[]> softIrqPaths = cpuQuarks.stream()
+                .map(quark -> {
+                    String[] cpuQuarkPath = ss.getFullAttributePathArray(quark);
+                    String[] irqAttributePath = new String[4];
+                    irqAttributePath[0] = cpuQuarkPath[0];
+                    irqAttributePath[1] = cpuQuarkPath[1];
+                    irqAttributePath[2] = Attributes.SOFT_IRQS;
+                    irqAttributePath[3] = String.valueOf(softIrqNumber);
+                    return irqAttributePath;
+                })
+                .collect(Collectors.toList());
+
+            ss.addAggregationRule(new AttributePriorityAggregationRule(ss, softIrqQuark, softIrqPaths));
+        }
+
     }
 }
