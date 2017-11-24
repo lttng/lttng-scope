2498c0a39e1182b34b3b37c71ddaa9f0bda3c138
Alexandre Montplaisir <alexmonthy@efficios.com>
2017-11-24 16:49:03 -0500

    Add column to event table to show matching filters
    Listeners galore!
    
    Signed-off-by: Alexandre Montplaisir <alexmonthy@efficios.com>

--- lttng-scope/src/main/kotlin/org/lttng/scope/views/events/EventTable.kt
+++ lttng-scope/src/main/kotlin/org/lttng/scope/views/events/EventTable.kt
@@ -17,11 +17,20 @@ import com.sun.javafx.scene.control.skin.VirtualFlow
 import javafx.application.Platform
 import javafx.beans.property.ReadOnlyObjectWrapper
 import javafx.collections.FXCollections
+import javafx.collections.ListChangeListener
 import javafx.scene.CacheHint
+import javafx.scene.Node
+import javafx.scene.control.TableCell
 import javafx.scene.control.TableColumn
 import javafx.scene.control.TableRow
 import javafx.scene.control.TableView
 import javafx.scene.layout.BorderPane
+import javafx.scene.layout.HBox
+import org.lttng.scope.project.ProjectFilters
+import org.lttng.scope.project.ProjectManager
+import org.lttng.scope.project.filter.EventFilterDefinition
+import org.lttng.scope.project.filter.getGraphic
+import org.lttng.scope.views.context.ViewGroupContextManager
 import org.lttng.scope.views.timecontrol.TimestampConversion
 
 /**
@@ -33,11 +42,12 @@ class EventTable(private val tableControl: EventTableControl) : BorderPane() {
 
     init {
         /* Setup the table */
-        val timestampCol = createColumn("Timestamp", 200.0, { TimestampConversion.tsToString(it.timestamp) })
-        val traceCol = createColumn("Trace", 100.0, { it.trace.name })
-        val cpuCol = createColumn("CPU", 50.0, { it.cpu.toString() })
-        val typeCol = createColumn("Event Type", 200.0, { it.eventName })
-        val fieldsCol = createColumn("Event Fields", 800.0, { event ->
+        val filterIconsCol = FilterColumn()
+        val timestampCol = createTextColumn("Timestamp", 200.0, { TimestampConversion.tsToString(it.timestamp) })
+        val traceCol = createTextColumn("Trace", 100.0, { it.trace.name })
+        val cpuCol = createTextColumn("CPU", 50.0, { it.cpu.toString() })
+        val typeCol = createTextColumn("Event Type", 200.0, { it.eventName })
+        val fieldsCol = createTextColumn("Event Fields", 800.0, { event ->
             event.fieldNames
                     .map { fieldName -> "$fieldName=${event.getField(fieldName, FieldValue::class.java)}" }
                     .toString()
@@ -47,7 +57,7 @@ class EventTable(private val tableControl: EventTableControl) : BorderPane() {
             fixedCellSize = 24.0
             isCache = true
             cacheHint = CacheHint.SPEED
-            columns.addAll(timestampCol, traceCol, cpuCol, typeCol, fieldsCol)
+            columns.addAll(filterIconsCol, timestampCol, traceCol, cpuCol, typeCol, fieldsCol)
 
             /* Row factory to add the click listener on each row */
             setRowFactory {
@@ -61,7 +71,7 @@ class EventTable(private val tableControl: EventTableControl) : BorderPane() {
         right = EventTableScrollToolBar(tableControl)
     }
 
-    private fun createColumn(headerText: String, initialWidth: Double, provideText: (TraceEvent) -> String): TableColumn<TraceEvent, String> {
+    private fun createTextColumn(headerText: String, initialWidth: Double, provideText: (TraceEvent) -> String): TableColumn<TraceEvent, String> {
         return TableColumn<TraceEvent, String>(headerText).apply {
             setCellValueFactory { ReadOnlyObjectWrapper(provideText(it.value)) }
             isSortable = false
@@ -134,5 +144,86 @@ class EventTable(private val tableControl: EventTableControl) : BorderPane() {
     private fun getVisibleRowIndices(): IntRange {
         val flow = (tableView.skin as TableViewSkin<*>).children[1] as VirtualFlow<*>
         return flow.firstVisibleCell.index..flow.lastVisibleCell.index
+    }
+}
+
+private class FilterColumn : TableColumn<TraceEvent, TraceEvent>(""), ProjectFilters.FilterListener {
+
+    private inner class FilterCell : TableCell<TraceEvent, TraceEvent>() {
+
+        private val filterSymbols = mutableMapOf<EventFilterDefinition, Node>()
+        private val node = HBox()
+
+        init {
+            enabledFilters.addListener(ListChangeListener<EventFilterDefinition> { c ->
+                if (c == null) return@ListChangeListener
+                while (c.next()) {
+                    for (removedFilter in c.removed) {
+                        filterSymbols.remove(removedFilter)?.let { node.children.remove(it) }
+                    }
+                    for (addedFilter in c.addedSubList) {
+                        val graphic = addedFilter.getGraphic()
+                        /*
+                         * Whenever a symbol is hidden, we don't want it to count in its
+                         * parent's layout calculations (so the HBox "collapses").
+                         */
+                        graphic.managedProperty().bind(graphic.visibleProperty())
+                        filterSymbols.put(addedFilter, graphic)
+                        graphic.isVisible = (item != null && addedFilter.predicate.invoke(item))
+                        node.children.add(graphic)
+                    }
+                }
+            })
+        }
+
+        override fun updateItem(item: TraceEvent?, empty: Boolean) {
+            super.updateItem(item, empty)
+            filterSymbols.forEach { filter, node ->
+                node.isVisible = (item != null && filter.predicate.invoke(item))
+            }
+            graphic = node
+        }
+    }
+
+    private val createdFilters = mutableListOf<EventFilterDefinition>()
+    private val enabledFilters = FXCollections.observableArrayList<EventFilterDefinition>()
+
+    init {
+        ViewGroupContextManager.getCurrent().currentTraceProject?.let {
+            ProjectManager.getProjectState(it).filters.registerFilterListener(this)
+        }
+        ViewGroupContextManager.getCurrent().currentTraceProjectProperty().addListener { _, _, newProject ->
+            //oldProject?. let { ProjectManager.getProjectState(it).filters.unregisterFilterListener(this) }
+            if (newProject != null) {
+                ProjectManager.getProjectState(newProject).filters.registerFilterListener(this)
+            } else {
+                createdFilters.clear()
+                enabledFilters.clear()
+            }
+        }
+
+        setCellValueFactory { ReadOnlyObjectWrapper(it.value) }
+        setCellFactory { FilterCell() }
+        isSortable = false
+        prefWidth = 20.0
+    }
+
+    override fun filterCreated(filter: EventFilterDefinition) {
+        createdFilters.add(filter)
+        if (filter.isEnabled) {
+            enabledFilters.add(filter)
+        }
+        filter.enabledProperty().addListener { _, _, nowEnabled ->
+            if (nowEnabled) {
+                enabledFilters.add(filter)
+            } else {
+                enabledFilters.remove(filter)
+            }
+        }
+    }
+
+    override fun filterRemoved(filter: EventFilterDefinition) {
+        createdFilters.remove(filter)
+        enabledFilters.remove(filter)
     }
 }
